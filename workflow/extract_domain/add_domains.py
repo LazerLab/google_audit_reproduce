df9f67108bc4bff5179caa9340ae174ffe40b6f6
zgvera <gzximo@gmail.com>
2024-09-24 21:21:14 -0400

    extract domains

---
+++ workflow/extract_domain/add_domains.py
@@ -0,0 +1,49 @@
+'''
+This file goes over all the parquet results and add a domain column.
+
+input: parquet file.
+output: updated parquet file. 
+'''
+
+import os
+import sys
+import tldextract
+import pandas as pd
+
+# Initialize tldextract with no live suffix update
+# This is for domain extraction
+no_fetch_extract = tldextract.TLDExtract(suffix_list_urls=None)
+    
+
+# Extract domains from urls
+def extract_domain(url):
+    # try:
+    #     return '.'.join(part for part in no_fetch_extract(url)[1:] if part)
+    
+    # Check if the URL is None or empty before proceeding
+    if not url:
+        return None
+    try:
+        extracted = no_fetch_extract(url)
+        # Correctly access the domain and suffix properties of the ExtractResult
+        domain_parts = [extracted.domain, extracted.suffix]
+        # Join the non-empty parts with '.'
+        return '.'.join(part for part in domain_parts if part)
+    
+    except Exception as e:
+        print(f"Error extracting domain from {url}: {e}")
+        return None
+
+    
+if __name__ == "__main__":
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+    
+
+    results = pd.read_parquet(input_dir)
+    
+    # extract domain
+    results['domain'] = results['url'].apply(extract_domain)
+    
+    results.to_parquet(output_dir, index=False)
+    print(output_dir)
